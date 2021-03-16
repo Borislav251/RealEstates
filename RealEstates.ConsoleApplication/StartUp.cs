58605238e33c2d59d8b94f71850a689f5326e6c7
Borislav251 <bolyuar@abv.bg>
2021-03-16 13:11:37 +0200

    Create DbContext and Services

--- RealEstates.ConsoleApplication/StartUp.cs
+++ RealEstates.ConsoleApplication/StartUp.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstates.Data;
+using RealEstates.Services;
+using System;
+using System.Text;
 
 namespace RealEstates.ConsoleApplication
 {
@@ -6,7 +10,24 @@ namespace RealEstates.ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var db = new RealEstateDbContext();
+            db.Database.Migrate();
+
+            IPropertiesService propertiesService = new PropertiesService(db);
+
+            //propertiesService.Create("Драгалевци", 100, 2019, 210000, "4-СТАЕН", "ЕПК", 20, 20);
+            //propertiesService.Create("Севера", 70, 1986, 50000, "2-СТАЕН", "ЕПК", 1, 8);
+
+            IDistrictsService districtService = new DistrictsService(db);
+            var districts = districtService.GetTopDistrictsByAveragePrice();
+
+            foreach (var district in districts)
+            {
+                Console.WriteLine($"{district.Name} => Price: {district.AveragePrice} ({district.MinPrice} - {district.MaxPrice}) => {district.PropertiesCount} properties");
+            }
+
         }
     }
 }
--- RealEstates.Models/RealEstatePropertyTag.cs
+++ RealEstates.Models/RealEstatePropertyTag.cs
@@ -1,11 +1,12 @@
-﻿namespace RealEstates.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstates.Models
 {
     public class RealEstatePropertyTag
     {
         public int PropertyId { get; set; }
 
         public virtual RealEstateProperty Property { get; set; }
-
 
         public int TagId { get; set; }
 
